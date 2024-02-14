13db07e5daee30b87db8f308c9f56da2b9083c6c
HXM <xiaomeng.huang.de@outlook.com>
2024-02-14 04:27:22 +0100

    methods for manipulation of database implemented

--- Docker/RobCar/database.py
+++ Docker/RobCar/database.py
@@ -1,42 +1,70 @@
-# Head 
-
-
-# Control 
+# Define Translation as array 
 forward_Velocity = float(0) 
 side_Velocity = float(0) 
 
 translation = [forward_Velocity, side_Velocity] # [0]: forward_Velocity ; [1]: side_Velocity 
 
+# Define rotation as array 
 yaw_Speed = float(0) 
 rotation = [yaw_Speed]  # [0]: yaw_Speed 
 
+# Control 
 Control = {} 
+Control["TimeStamp"] = float(0) # TODO: TimeStamp needs to be added 
 Control["Translation"] = translation
 Control["Rotation"] = rotation 
 
+# Feedback 
 Feedback = {} 
+Feedback["TimeStamp"] = float(0) # TODO: TimeStamp needs to be added 
 Feedback["Translation"] = translation
 Feedback["Rotation"] = rotation 
+
 
 class database: 
     def __init__(self):
         self.ctrl_data = Control 
-        self.feedback_data = Feedback
-        
-  
+        self.feedback_data = Feedback 
+    
+    
+    # Methods for data collection, e.g. from ROS or MQTT 
+    ## forward_Velocity 
+    def write_forward_Velocity(self, _forward_Velocity): 
+        # TODO: TimeStamp needs to be written 
+        self.ctrl_data["Translation"][0] = float(_forward_Velocity) 
+    
+    ## yaw_Speed 
+    def write_yaw_Speed(self, _yaw_Speed): 
+        # TODO: TimeStamp needs to be written 
+        self.ctrl_data["Rotation"][0] = float(_yaw_Speed) 
+    
+    
+    # Methods for data Selection, e.g. useful for the control of robcar 
+    ## forward_Velocity 
+    def read_forward_Velocity(self): 
+        # TODO: TimeStamp needs to be qualified 
+        return self.ctrl_data["Translation"][0] 
+    
+    ## yaw_Speed 
+    def read_yaw_Speed(self): 
+        # TODO: TimeStamp needs to be qualified 
+        return self.ctrl_data["Rotation"][0] 
+
+ 
+
 def test_db(_db):
-    forward_vel = _db.ctrl_data["Translation"][0] 
-    _db.feedback_data["Translation"][0] = forward_vel 
+    _db.write_forward_Velocity(2.0) 
+    _db.write_yaw_Speed(4.0) 
+    
+    print(_db.read_forward_Velocity())
+    print(_db.read_yaw_Speed())
     
 
 
 def main():
     db = database() 
     
-    db.ctrl_data["Translation"][0] = 3.0 
     test_db(db) 
-    
-    print(db.feedback_data["Translation"][0]) 
 
 
 
