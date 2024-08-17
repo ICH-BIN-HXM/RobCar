335fc5515e44844844af83568c94061ed3cf9b51
HXM <xiaomeng.huang.de@outlook.com>
2024-08-17 20:49:23 +0200

    Control Layer implemented

--- Docker/raspi/Volume/RobCar/Parameters.py
+++ Docker/raspi/Volume/RobCar/Parameters.py
@@ -2,3 +2,10 @@ LEFT_FRONT_WHEEL = 0
 RIGHT_FRONT_WHEEL = 1
 LEFT_REAR_WHEEL = 2
 RIGHT_REAR_WHEEL = 3
+
+flag_simulation = True
+
+pwm_increment = 1
+
+control_frequency = 0.1
+simulation_time_step = control_frequency / 100.0
